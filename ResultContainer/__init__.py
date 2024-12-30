e2b4b7bda5b32a68c972a8625004875bdcf24a2f
Scott Boyce <seboyce@usgs.gov>
2024-12-30 09:21:31 -0800

    fix: update pyproj.toml to correct __version__ location

--- ResultContainer/__init__.py
+++ ResultContainer/__init__.py
@@ -78,7 +78,7 @@ __license__ = "MIT"
 __status__ = "Development"  # set to "Prototype", "Development", "Production"
 __url__ = "https://github.com/ScottBoyce-Python/ResultContainer"
 __description__ = "ResultContainer is a Result class that mimics the behavior of Rust's Result enum that wraps values in an Ok(value) and Err(e) variant. Math operations, attributes, and methods are passed to value in Ok(value). If an operation with the Ok(value) variant results in an error, then it is converted to an Err(e) variant. Err(e) contains one or more error messages and math operations, attributes, and methods result in appending the respective errors."
-__copyright__ = "Copyright (c) 2024 Scott E. Boyce"
+__copyright__ = "Copyright (c) 2025 Scott E. Boyce"
 
 __all__ = ["Result", "Ok", "Err", "ResultErr"]
 
